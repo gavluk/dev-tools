2a74f20486ed39a9bd446e8bd70cdecef8dd9b82
gavluk <gavluk@teamharbour.com>
2020-05-30 13:46:03 +0300

    Kafka docker run refinement

--- docker-cmd.sh
+++ docker-cmd.sh
@@ -2,9 +2,10 @@
 
 IMAGE=$1
 CMD=$2
+CMD2=$3
 
 echo "Running docker $IMAGE command '$CMD' into current directory"
 
-docker run --rm -u `id -u`:`id -g` -v `pwd`:/work -w /work $IMAGE $CMD
+docker run --rm -u `id -u`:`id -g` -v `pwd`:/work -w /work $IMAGE $CMD $CMD2
 
 
--- docker-kafka-run.sh
+++ docker-kafka-run.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
 docker network create kafka-net
-docker run -d --name zookeeper --network kafka-net jplock/zookeeper
-docker run -d --name kafka --network kafka-net --env ZOOKEEPER_IP=zookeeper ches/kafka
+
+docker run -d --name zookeeper \
+    --network kafka-net \
+    jplock/zookeeper
+
+sleep 2
+
+docker run -d --name kafka \
+    -p 9092:9092 -p 7203:7203 \
+    --network kafka-net \
+    --env KAFKA_ADVERTISED_HOST_NAME=127.0.0.1 \
+    --env ZOOKEEPER_IP=zookeeper \
+    ches/kafka
+
+sleep 5
 
 # create some topic
 docker run --rm --network kafka-net ches/kafka kafka-topics.sh \
@@ -12,6 +25,7 @@ docker run --rm --network kafka-net ches/kafka kafka-topics.sh \
 --zookeeper zookeeper
 
 # list of topics
+docker run --rm --network kafka-net ches/kafka sh -c unset JMX_PORT
 docker run --rm --network kafka-net ches/kafka kafka-topics.sh --list --zookeeper zookeeper
 
 # more info at: https://hub.docker.com/r/ches/kafka/
